de7656e8260a468ba3765b87bf89b37e5d870dcd
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-25 13:19:05 +0100

    Add ReactorThreadMain class for reactive employee processing

---
+++ employees-se/src/main/java/employees/ReactorThreadMain.java
@@ -0,0 +1,22 @@
+package employees;
+
+import reactor.core.publisher.Flux;
+
+public class ReactorThreadMain {
+
+    //https://www.jtechlog.hu/2024/02/01/project-reactor-szalkezeles.html
+    public static void main(String[] args) {
+        Flux.just(
+                        new Employee("John", 1999),
+                        new Employee("Jane", 1999),
+                        new Employee("Doe", 1999)
+                )
+                .doOnNext(e -> System.out.println("Filtering: " + e + " on thread " + Thread.currentThread().getName()))
+                .filter(emp -> emp.yearOfBirth() < 2000)
+                .doOnNext(e -> System.out.println("Mapping: " + e + " on thread " + Thread.currentThread().getName()))
+                .map(Employee::name)
+                .doOnNext(e -> System.out.println("uppercasing: " + e + " on thread " + Thread.currentThread().getName()))
+                .map(String::toUpperCase)
+                .subscribe(System.out::println);
+    }
+}
