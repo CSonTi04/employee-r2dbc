1c6049e6896a4d63f1afd455a09c6faa10605e19
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-25 11:53:12 +0100

    Add custom query method to EmployeeRepository for finding employees by name

--- employees-mongodb/src/main/java/employees/EmployeeRepository.java
+++ employees-mongodb/src/main/java/employees/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package employees;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,4 +10,10 @@ public interface EmployeeRepository extends ReactiveMongoRepository<Employee, St
     Flux<EmployeeDto> findAllBy();
 
     <T> Mono<T> findDtoById(String id, Class<T> clazz);
+
+    @Query("""
+            { 'name': ?0, 'id': { $ne: null } }
+    """)
+    Flux<EmployeeDto> findByName(String name);
+
 }
