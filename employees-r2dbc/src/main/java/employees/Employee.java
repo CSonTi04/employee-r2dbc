c90b53f2b861848e04358d00ef339525ffa36a0c
Tóth András <tibor.andras.toth@idomsoft.hu>
2025-11-25 10:02:20 +0100

    Annotate Employee class for R2DBC mapping and update application properties for migration resources

--- employees-r2dbc/src/main/java/employees/Employee.java
+++ employees-r2dbc/src/main/java/employees/Employee.java
@@ -2,11 +2,16 @@ package employees;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
+@Table(name = "employees")
 public class Employee {
 
+
+    @Id
     private Long id;
 
     private String name;
